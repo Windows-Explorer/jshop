8806eaed08dc5dc8e4844dd80874f7a3d6305f6f
Windows-Explorer <danil.perfilef@yandex.ru>
2022-11-17 01:03:53 +0800

    init

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+import { UsersService } from './users.service'
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: UsersService
+  let repository: Repository<User>
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+  beforeEach(() => {
+    service = new UsersService(repository)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const result: Promise<User[]> = new Promise()
+
+      jest.spyOn(service, 'findAll').mockImplementation(() => result)
+
+      expect(await service.findAll()).toBe(result)
+    })
+  })
+})

a064d4f5f7e077012369f4d800e9855c96b355fd
Windows-Explorer <danil.perfilef@yandex.ru>
2022-11-16 22:59:07 +0800

    Removed validation in dto class

--- src/auth/dto/user-create.dto.ts
+++ src/auth/dto/user-create.dto.ts
@@ -1,14 +1,5 @@
-import { IsString, IsEmail, Length} from "class-validator"
-
 export class UserCreateDto {
-    @IsString()
-    @Length(4, 32)
     username: string
-
-    @IsEmail()
     email: string
-
-    @IsString()
-    @Length(8, 16)
     password: string
 }

b1b52f51131deedd64c06773af00029b6c5ff1cb
Windows-Explorer <danil.perfilef@yandex.ru>
2022-11-26 08:06:42 +0800

    Added KAFKA-producer

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,7 +1,40 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common'
+import { Client, ClientKafka, Transport } from '@nestjs/microservices'
 
-@Controller("/")
+@Controller('auth')
 export class AppController {
-    constructor() {}
+
+    @Client({
+        transport: Transport.KAFKA,
+        options: {
+            client: {
+                clientId: "gateway-producer",
+                brokers: [`192.168.43.74:9092`],
+            },
+            consumer: {
+                groupId: "auth-consumer"
+            },
+            
+        }
+    })
+
+    client: ClientKafka
+
+    async onModuleInit() {
+        this.client.subscribeToResponseOf("post.auth.signUp")
+        this.client.subscribeToResponseOf("post.auth.signIn")
+
+        await this.client.connect()
+    }
+
+    @Get("/signup")
+    async signUp(@Body() message: any): Promise<any> {
+        return this.client.send("get.products.findAll", message)
+    }
+
+    @Post("/signin")
+    async signIn(@Body() message: any): Promise<any> {
+        return this.client.send("post.products.create", message)
+    }
     
 }
--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { ConfigModule } from "@nestjs/config"
+import { AuthService } from './auth/auth.service'
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { ConfigModule } from "@nestjs/config"
       isGlobal: true
     })
   ],
-  controllers: [AppController]
+  controllers: [AppController],
+  providers: [AuthService]
 })
 
 export class AppModule {}

64ee242b6fee8ca1043b0bc1362003f7c45194f0
Windows-Explorer <danil.perfilef@yandex.ru>
2022-11-30 18:33:46 +0800

    Removed comments

--- authentification-service/src/app.controller.ts
+++ authentification-service/src/app.controller.ts
@@ -3,16 +3,13 @@ import { MessagePattern, Payload, RpcException } from '@nestjs/microservices'
 import { AuthService } from './auth/auth.service'
 import { UserCreateDto } from './auth/dto/user-create.dto'
 import { IResult } from './dto/result.dto'
-import { AllExceptionsFilter } from './extentions/all.exception-filter'
 
 @Controller("/")
 export class AppController {
     constructor(private readonly authService: AuthService) {}
 
-    // @UseFilters(new AllExceptionsFilter())
     @MessagePattern("post.auth.signUp")
     async signUp(@Payload() userDto: UserCreateDto): Promise<any> {
-        console.log(userDto)
         const user = await this.authService.signUp(userDto)
 
         const result: IResult = { data: await this.authService.signUp(userDto), error: { statusCode: HttpStatus.OK, message: "OK" }}

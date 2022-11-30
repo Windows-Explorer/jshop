7caff34a87a9b841f9a15a7076767401b2fee535
Windows-Explorer <danil.perfilef@yandex.ru>
2022-11-30 22:16:21 +0800

    Fixed response payload by auth-service

--- authentification-service/src/unique/unique.controller.ts
+++ authentification-service/src/unique/unique.controller.ts
@@ -4,20 +4,20 @@ import { IResult } from 'src/dto/result.dto'
 import { UniqueDto } from 'src/dto/unique.dto'
 import { UsersService } from 'src/users/users.service'
 
-@Controller('/')
+@Controller('')
 export class UniqueController {
     constructor(private readonly userService: UsersService) {}
 
     @MessagePattern("get.unique.email")
     async isEmailUnique(@Payload() uniqueDto: UniqueDto): Promise<IResult> {
-        return { data: await this.userService.findByEmail(uniqueDto.message), error: {message: "OK", statusCode: HttpStatus.OK} }
+        return { data: await (await this.userService.findByEmail(uniqueDto.message)).email, error: {message: "OK", statusCode: HttpStatus.OK} }
     }
 
     
     @MessagePattern("get.unique.username")
     async isUsernameUnique(@Payload() uniqueDto: UniqueDto): Promise<IResult> {
         try {
-            return { data: await this.userService.findByUsername(uniqueDto.message), error: {message: "OK", statusCode: HttpStatus.OK}  }
+            return { data: await (await this.userService.findByUsername(uniqueDto.message)).username, error: {message: "OK", statusCode: HttpStatus.OK}  }
         }
         catch {
             return { data: null, error: {message: "OK", statusCode: HttpStatus.OK}}

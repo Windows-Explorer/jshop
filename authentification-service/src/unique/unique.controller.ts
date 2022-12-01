bc3077740bf8ec2ff5bf3dcbf17173f96c845d80
Windows-Explorer <danil.perfilef@yandex.ru>
2022-12-01 18:54:37 +0800

    Fixed unique validation at auth-service

--- authentification-service/src/unique/unique.controller.ts
+++ authentification-service/src/unique/unique.controller.ts
@@ -9,18 +9,23 @@ export class UniqueController {
     constructor(private readonly userService: UsersService) {}
 
     @MessagePattern("get.unique.email")
-    async isEmailUnique(@Payload() uniqueDto: UniqueDto): Promise<IResult> {
-        return { data: await (await this.userService.findByEmail(uniqueDto.message)).email, error: {message: "OK", statusCode: HttpStatus.OK} }
+    async isEmailUnique(@Payload() uniqueDto: string): Promise<IResult> {
+        try {
+            return { data: (await this.userService.findByEmail(uniqueDto)).email, error: { message: "OK", statusCode: HttpStatus.OK } }
+        }
+        catch {
+            return { data: null, error: { message: "OK", statusCode: HttpStatus.OK } }
+        }
     }
 
     
     @MessagePattern("get.unique.username")
-    async isUsernameUnique(@Payload() uniqueDto: UniqueDto): Promise<IResult> {
+    async isUsernameUnique(@Payload() uniqueDto: string): Promise<IResult> {
         try {
-            return { data: await (await this.userService.findByUsername(uniqueDto.message)).username, error: {message: "OK", statusCode: HttpStatus.OK}  }
+            return { data: (await this.userService.findByUsername(uniqueDto)).username, error: { message: "OK", statusCode: HttpStatus.OK }  }
         }
         catch {
-            return { data: null, error: {message: "OK", statusCode: HttpStatus.OK}}
+            return { data: null, error: { message: "OK", statusCode: HttpStatus.OK } }
         }
     }
 }

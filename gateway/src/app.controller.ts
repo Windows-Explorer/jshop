311e6695e4d359f0076b8f35f47a88ef5002e9d8
Windows-Explorer <danil.perfilef@yandex.ru>
2022-12-04 15:32:19 +0800

    Now it's return not a string)

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, Post, Body, HttpCode, Res, HttpStatus, UseGuards } from '@nestjs/common'
-import { Client, ClientKafka, Transport } from '@nestjs/microservices'
-import { response, Response } from 'express'
 import { AuthGuard } from './auth/guards/auth.guard'
-import { IResult } from './dto/result.dto'
 
 @Controller('')
 export class AppController {
 
     @UseGuards(AuthGuard)
     @Get("/")
-    async index(): Promise<string> {
-        return "string"
+    async index(): Promise<any> {
+        return { data: "data" }
     }
 }

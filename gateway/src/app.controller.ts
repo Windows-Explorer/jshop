db4b64f94b80e2250e301bb9ce5d5e05efe31260
Windows-Explorer <danil.perfilef@yandex.ru>
2022-12-05 16:26:26 +0800

    Code cleaning

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, HttpCode, Res, HttpStatus, UseGuards } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
 import { AuthGuard } from './guards/auth.guard'
 
 @Controller('')

1ebae9ad0d64b1bd0b3e0c560922b55f16459790
Windows-Explorer <danil.perfilef@yandex.ru>
2022-12-14 11:23:14 +0800

    Removed Fastify Adapter because it's not given perfomance boost

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,10 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify"
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  const app = await NestFactory.create(AppModule)
   app.enableCors()
   console.log(app.get(ConfigService))
   await app.listen(3000)

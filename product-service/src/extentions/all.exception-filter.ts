51d289c8a2da687c3ede6e0fef96b09e508501a2
Windows-Explorer <danil.perfilef@yandex.ru>
2023-01-14 19:05:19 +0800

    Fixed all exception filter IResulter (now IOutput)

--- product-service/src/extentions/all.exception-filter.ts
+++ product-service/src/extentions/all.exception-filter.ts
@@ -1,11 +1,11 @@
 import { Catch, RpcExceptionFilter, ArgumentsHost, HttpException, Inject } from "@nestjs/common"
 import { RESULTER_TOKEN } from "src/common/constants/inject-tokens.constant"
 import { IResult } from "src/common/interfaces/result.interface"
-import { IResulter } from "src/common/interfaces/resulter.interface"
+import { IOutput } from "src/common/interfaces/resulter.interface"
 
 @Catch()
 export class AllExceptionsFilter implements RpcExceptionFilter<any> {
-  constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IResulter) {}
+  constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IOutput) {}
   catch(exception: HttpException, host: ArgumentsHost): any {
 
     const result: IResult<any> = this._resulter.response(exception.getStatus(), exception.message)

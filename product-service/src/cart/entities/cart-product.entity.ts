37f514e90d02ce8e6b09c7d0b527f961c3868e8c
Windows-Explorer <danil.perfilef@yandex.ru>
2023-05-02 13:03:52 +0800

    CartProduct includes product entity now

--- product-service/src/cart/entities/cart-product.entity.ts
+++ product-service/src/cart/entities/cart-product.entity.ts
@@ -1,35 +1,17 @@
 import { ICartProduct } from "src/common/interfaces/data/cart-product.interface"
-import { ICategory } from "src/common/interfaces/data/category.interface"
-import { ISubcategory } from "src/common/interfaces/data/subcategory.interface"
-import { Category } from "src/products/entities/category.entity"
-import { Subcategory } from "src/products/entities/subcategory.entity"
+import { IProduct } from "src/common/interfaces/data/product.interface"
+import { Product } from "src/products/entities/product.entity"
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class CartProduct implements ICartProduct {
-    constructor() {
+    constructor() { }
 
-    }
     @PrimaryGeneratedColumn()
     id?: number
 
-    @Column({ length: 255, type: "varchar", unique: false, nullable: false })
-    title: string
-
-    @Column({ length: 1024, type: "varchar", unique: false, nullable: false })
-    description: string
-
-    @Column({ length: 255, type: "varchar", unique: false, nullable: false })
-    image?: string
-
-    @Column({ length: 255, type: "float", unique: false, nullable: false })
-    cost: number
-
-    @ManyToOne(() => Category, (type: ICategory) => type.id, { eager: true, cascade: true })
-    category: ICategory
-
-    @ManyToOne(() => Subcategory, (type: ISubcategory) => type.id, { eager: true, cascade: true, nullable: true })
-    subcategory?: ISubcategory
+    @ManyToOne(() => Product, (type: IProduct) => type.id, { eager: true, cascade: true, nullable: false })
+    product: IProduct
 
     @Column({ length: 255, type: "int", unique: false, nullable: false })
     count: number
--- product-service/src/common/interfaces/data/cart-product.interface.ts
+++ product-service/src/common/interfaces/data/cart-product.interface.ts
@@ -1,5 +1,6 @@
 import { IProduct } from "./product.interface"
 
-export interface ICartProduct extends IProduct {
+export interface ICartProduct {
+    product: IProduct
     count: number
 }

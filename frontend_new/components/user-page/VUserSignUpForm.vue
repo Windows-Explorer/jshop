6b1061d4ffcc74d484f9a92782fc3052f8bd2f8d
Windows-Explorer <danil.perfilef@yandex.ru>
2023-05-10 21:26:29 +0800

    Added redirect to register submit

--- frontend_new/components/user-page/VUserSignUpForm.vue
+++ frontend_new/components/user-page/VUserSignUpForm.vue
@@ -71,6 +71,7 @@ import { Ref, ref } from "vue"
 import { IUserSignUp } from "../../common/interfaces/user.interface"
 import { Validator } from "../../common/validator/validator"
 import useAuthStore from "../../store/auth.store"
+import { useRouter } from "nuxt/app"
 
 const userIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H18C18 18.6863 15.3137 16 12 16C8.68629 16 6 18.6863 6 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11Z"></path></svg>'
 const emailIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z"></path></svg>'
@@ -87,6 +88,8 @@ const inputClasses = {
 }
 
 const validator = new Validator()
+const router = useRouter()
+const authStore = useAuthStore()
 
 async function uniqueUsername(): Promise<boolean> {
     return await validator.isUsernameUnique(String(user.value.username))
@@ -111,10 +114,9 @@ async function submitSignUpForm() {
     signUpForm.value.node.submit()
 }
 
-const authStore = useAuthStore()
-
 async function onSubmit() {
     await authStore.signUp(user.value)
+    await router.push({ path: "/" })
 }
 
 </script>

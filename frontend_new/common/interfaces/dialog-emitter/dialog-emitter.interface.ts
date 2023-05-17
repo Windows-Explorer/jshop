7c763d48b72e95fc0d57d54b0aba8fa9072c4db5
Windows-Explorer <danil.perfilef@yandex.ru>
2023-05-18 00:14:23 +0800

    Removed old popup system

--- frontend_new/common/interfaces/dialog-emitter/dialog-emitter.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IDialogProps } from "../props/dialog.props.interface"
-
-export interface IDialogEmitter {
-    open(props?: IDialogProps): Promise<void>
-    close(): Promise<void>
-}
--- frontend_new/common/interfaces/props/dialog.props.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IDialogActionsProps } from "./dialog.actions.props.interface"
-
-export interface IDialogProps {
-    dialogTitle?: string,
-    message?: string,
-    actions?: IDialogActionsProps
-}

b6a348293386f26b6baebe584734917069a43b3d
Windows-Explorer <danil.perfilef@yandex.ru>
2023-05-18 22:43:04 +0800

    Add swagger module

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,23 @@
 import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle("API Swagger")
+    .setDescription("API Gateway methods")
+    .setVersion("0.1")
+    .addTag("API")
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
+
   console.log(app.get(ConfigService))
   await app.listen(3000)
 }

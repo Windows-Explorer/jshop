db60708be9157046e039096c10f90a25d8be8d5c
Windows-Explorer <danil.perfilef@yandex.ru>
2023-05-29 17:46:30 +0800

    it so stupid shit

--- gateway/src/products/categories.controller.ts
+++ gateway/src/products/categories.controller.ts
@@ -3,13 +3,15 @@ import { ClientKafka } from "@nestjs/microservices"
 import { Response } from "express"
 import { PRODUCTS_KAFKA_CLIENT_TOKEN } from "src/common/constants/inject-tokens.constants"
 
-@Controller("products")
+@Controller("products/categories")
 export class CategoriesController {
     constructor(@Inject(PRODUCTS_KAFKA_CLIENT_TOKEN) private readonly _client: ClientKafka) { }
 
-    @Get("/dolboebizm")
+    @Get("/")
     async findAll(@Res() response: Response) {
+        console.log("findALl categories has been called")
         const result = await this._client.send("products.categories.findAll", "").toPromise()
+        console.log(result)
         response.status(result.statusCode).send(result.message)
     }
 }

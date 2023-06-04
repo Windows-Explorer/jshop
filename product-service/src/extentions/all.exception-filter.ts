0bff9f6a42ff12ca3858ea37b6bf776f55e9f20a
Windows-Explorer <danil.perfilef@yandex.ru>
2023-06-04 19:12:39 +0800

    da

--- product-service/src/extentions/all.exception-filter.ts
+++ product-service/src/extentions/all.exception-filter.ts
@@ -5,11 +5,16 @@ import { IOutput } from "src/common/interfaces/resulter.interface"
 
 @Catch()
 export class AllExceptionsFilter implements RpcExceptionFilter<any> {
-  constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IOutput) {}
+  constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IOutput) { }
   catch(exception: HttpException, host: ArgumentsHost): any {
 
-    const result: IResult<any> = this._resulter.response(exception.getStatus(), exception.message)
-
-    return result
+    try {
+      const result: IResult<any> = this._resulter.response(exception.getStatus(), exception.message)
+      return result
+    }
+    catch (error) {
+      const result: IResult<any> = this._resulter.response(500, "Internal error")
+      return result
+    }
   }
 }

927728128474a186826cebee3797edc58c889dd0
Windows-Explorer <danil.perfilef@yandex.ru>
2023-06-14 16:27:06 +0800

    Added mariadb volume

---
+++ content-generator/src/data/categoryNames.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categoryNames = void 0;
+exports.categoryNames = [
+    "Lavazza", "Maxwell", "Douwe Egberts", "Don Francisco", "Findlays Finest", "Nescafe"
+];
---
+++ content-generator/src/params.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ParamsAPI = void 0;
+exports.ParamsAPI = {
+    api_host: "https://api.stoichev.apt0.ru"
+};

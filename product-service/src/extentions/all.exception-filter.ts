86e903a5c6296680802ab902b85be2e6363efc0e
Windows-Explorer <danil.perfilef@yandex.ru>
2023-06-20 13:14:12 +0800

    AAA

--- product-service/src/extentions/all.exception-filter.ts
+++ product-service/src/extentions/all.exception-filter.ts
@@ -8,13 +8,9 @@ export class AllExceptionsFilter implements RpcExceptionFilter<any> {
   constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IOutput) { }
   catch(exception: HttpException, host: ArgumentsHost): any {
 
-    try {
+    
       const result: IResult<any> = this._resulter.response(exception.getStatus(), exception.message)
       return result
-    }
-    catch (error) {
-      const result: IResult<any> = this._resulter.response(500, error)
-      return result
-    }
+    
   }
 }

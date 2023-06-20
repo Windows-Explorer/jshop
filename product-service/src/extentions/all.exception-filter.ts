412fface64f1e923852353b4a4df9066814630f8
Windows-Explorer <danil.perfilef@yandex.ru>
2023-06-20 13:30:33 +0800

    help

--- product-service/src/extentions/all.exception-filter.ts
+++ product-service/src/extentions/all.exception-filter.ts
@@ -8,12 +8,11 @@ export class AllExceptionsFilter implements RpcExceptionFilter<any> {
   constructor(@Inject(RESULTER_TOKEN) private readonly _resulter: IOutput) { }
   catch(exception: HttpException, host: ArgumentsHost): any {
 
-    const status = exception.getStatus()
-    if (status) {
-      const result: IResult<any> = this._resulter.response(status, exception.message)
+    try {
+      const result: IResult<any> = this._resulter.response(exception.getStatus(), exception.message)
       return result
     }
-    else {
+    catch (error) {
       const result: IResult<any> = this._resulter.response(500, exception.message)
       return result
     }
